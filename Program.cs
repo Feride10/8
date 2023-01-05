3df7d19d114e883d6145c11a4dac29b2d6374000
Feride_queen <faridka0@gmail.com>
2023-01-05 16:00:23 +0300

    54,56,58 задачи

---
+++ Program.cs
@@ -0,0 +1,162 @@
+﻿//54
+
+void InputMatrix(int[,] matrix)
+	{
+	    for (int i = 0; i < matrix.GetLength(0); i++)
+	    {
+	        for (int j = 0; j < matrix.GetLength(1); j++)
+	            matrix[i, j] = new Random().Next(-10, 11); // [-10, 10]
+	    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+
+void ReleaseMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Начальный двумерный массив: ");
+PrintMatrix(matrix);
+Console.WriteLine();
+ReleaseMatrix(matrix);
+Console.WriteLine("Конечный двумерный массив: ");
+PrintMatrix(matrix);
+
+
+//56
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+
+int ReleaseMatrix(int[,] matrix)
+{
+    int minRow = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+        minRow += matrix[0, i];
+
+    for (int i = 1; i < matrix.GetLength(0); i++)
+    {
+        int sumRow = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sumRow += matrix[i, j];
+        }
+        if (minRow > sumRow)
+            minRow = sumRow;
+    }
+    return minRow;
+}
+
+
+Console.Clear();
+Console.Write("Введите размер прямоугольного двумерного массива: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+while (size[0] == size[1])
+{
+    Console.Write("Вы ошиблись!\nВведите размер прямоугольного двумерного массива: ");
+    size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+}
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Начальный двумерный массив: ");
+PrintMatrix(matrix);
+Console.WriteLine();
+Console.WriteLine($"Результат: {ReleaseMatrix(matrix)}");
+
+
+
+//58
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+
+int[,] ReleaseMatrix(int[,] FirstMatrix, int[,] SecondMatrix)
+{
+    int[,] result = new int[FirstMatrix.GetLength(0), FirstMatrix.GetLength(1)];
+    for (int i = 0; i < FirstMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < FirstMatrix.GetLength(1); j++)
+            result[i, j] = FirstMatrix[i, j] * SecondMatrix[i, j];
+    }
+    return result;
+}
+
+
+Console.Clear();
+Console.Write("Введите размер матриц: "); // размеры матриц должны быть одинаковые
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] FirstMatrix = new int[size[0], size[1]];
+int[,] SecondMatrix = new int[size[0], size[1]];
+InputMatrix(FirstMatrix);
+InputMatrix(SecondMatrix);
+Console.WriteLine("Начальный массив 1: ");
+PrintMatrix(FirstMatrix);
+Console.WriteLine("Начальный массив 2: ");
+PrintMatrix(SecondMatrix);
+Console.WriteLine("Результат:");
+PrintMatrix(ReleaseMatrix(FirstMatrix, SecondMatrix));
